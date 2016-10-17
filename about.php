27a287217c90970d4e4bcce9549fd23a0b583563
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-10-17 21:24:24 +0530

    1

--- about.php
+++ about.php
@@ -320,7 +320,7 @@
          <div class="row">
             <div class="span12">
                <div class="copyright pull-left">
-                  <span><a href="www.hashinc.in"><img src="images/logo.png"></a></span> 2016 | ALL RIGHTS RESERVED
+                  <span><a href="www.hashinc.in"></a></span> 2016 | ALL RIGHTS RESERVED
                </div>
                <div class="bottom-links pull-right">
                   <a href="index.php">HOME</a> |
