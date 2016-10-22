b73698b4b90e6594a60108df45a3152b980f6093
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-10-22 12:47:34 +0530

    1

--- portfolio.php
+++ portfolio.php
@@ -88,8 +88,8 @@
 						<ul class="menu">
                            <li><a href="index.php">Home</a>
                            <li><a href="about.php">About Us</a>
-                           <li><a class="active" href="#">Services</a></li>
-                           <li><a href="portfolio.php">Our Work</a></li>
+                           <li><a href="services.php">Services</a></li>
+                           <li><a class="active" href="#">Our Work</a></li>
                            <li><a href="contact.php">Contact</a></li>
                            <li><a href="blog.php">Blog</a></li>
                         </ul><!-- /.menu -->
