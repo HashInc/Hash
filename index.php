49be9bb6486e4cab059ffa2f59cf65a6b05613ea
arihant927 <arihantchamp927@gmail.com>
2017-01-27 19:08:59 +0530

    ddgh

--- index.php
+++ index.php
@@ -186,7 +186,7 @@
             <div class="span12">
                <div class="titlebox Hashinc-animation" data-animation="fadeInUp" data-animation-delay="0" data-animation-offset="75%">
                   <h2 class="subtitle text-white">Testimonials</h2>
-                  <h3 class="maintitle text-color">WHAT OUR Founders SAY?</h3>
+                  <h3 class="maintitle text-color">What our founders day?</h3>
                   <p class="text-white">Let us see what our founders say about HashInclude.</p>
                </div>
                <div class="spacer"></div>
