5edec5e92d4f4e79c2db0a70fa8ca87b43954d13
arihant927 <arihantchamp927@gmail.com>
2017-01-30 14:41:05 +0530

    changes in store

--- store.php
+++ store.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
-   <!-- Basic Page Needs -->
+
    <meta charset="utf-8">
-    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
    <title>HashInclude | Remember The Name</title>
    <?php 
 		$number = rand(1, 10);
@@ -21,40 +21,28 @@
 }
 </style>
 
-   <!-- Mobile Specific Metas -->
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-   <!-- Bootstrap  -->
    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
 
-   <!-- Theme Style -->
    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
 
-   <!-- Colors -->
    <link rel="stylesheet" type="text/css" href="stylesheets/colors/color4.css" id="colors">
    
-   <!-- Animation Style -->
    <link rel="stylesheet" type="text/css" href="stylesheets/animate.css">
 
-   <!-- Google Fonts -->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
 
-   <!-- Favicon and touch icons  -->
    <link href="icon/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144">
    <link href="icon/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114">
    <link href="icon/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72">
    <link href="icon/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon-precomposed">
    <link href="icon/favicon.png" rel="shortcut icon">
 
-   <!--[if lt IE 9]>
-      <script src="javascript/html5shiv.js"></script>
-      <script src="javascript/respond.min.js"></script>
-   <![endif]-->
 </head>
 
 <body>
-   <!-- Top -->
       <div class="se-pre-con"></div>
    <div class="top">
       <div class="container">
@@ -63,12 +51,10 @@
                <ul class="contact-info inline pull-left">
                   <li class="email"><a href="mailto:support@hashinc.in">support@hashinc.in</a></li>
                </ul>
-            </div><!-- /.span8 -->
-            </div><!-- /.row -->
-      </div><!-- /.containter -->
+            </div>
+            </div>
+      </div>
    </div>
-
-   <!-- Parallax Fullwidth -->
    <div id="page-slider" class="parallax-bg7 parallax">
       <div class="overlay"></div>
       <header id="header" class="header">
@@ -80,10 +66,10 @@
                         <a href="./" rel="home">
                            <img src="images/logo.png" alt="Hash Include" />
                         </a>
-                     </div><!-- /.logo -->
-                  </div><!-- /.span2 -->
+                     </div>
+                  </div>
                   <div class="span10">
-                     <div class="btn-menu"></div><!-- //mobile menu button -->
+                     <div class="btn-menu"></div>
                      <nav id="mainnav" class="mainnav">
                         <ul class="menu">
                            <li><a href="index.php">Home</a>
@@ -92,33 +78,29 @@
                            <li><a href="ourwork.php">Our Work</a></li>
                            <li><a href="contact.php">Contact</a></li>
                            <li><a class="active" href="#">YOUR STORE</a></li>
-                        </ul><!-- /.menu -->
+                        </ul>
                      </nav>
-                  </div><!-- /.span10 -->
-               </div><!-- /.row -->
-            </div><!-- /.container -->
-         </div><!-- /.header-wrap -->
+                  </div>
+               </div>
+            </div>
+         </div>
       </header>
    </div>
-
-   <!-- Page Title -->
    <section class="page-title-container">
    	<div class="container page-title">
    		<div class="page-title-wrapper">
 			   <div class="span6 page-title-captions">
 			   	<h1>HashCart</h1>
-			   </div><!-- /.span6 -->
+			   </div>
 			   <div class="span6 breadcrumbs">
 					<ul>
 					<li><a href="index.php">Home </a>/</li>
 					<li><a href="Store.php">Your Store </a>/</li>
 					</ul>
-			   </div><!-- /.span6 -->
+			   </div>
          </div>
-      </div><!-- /.container -->
+      </div>
    </section>
-
-   <!-- Blog Posts -->
    <section class="section blog-posts" style="background-color: #10101d">
       <div class="container">
          <div class="row">
@@ -129,12 +111,10 @@
      
                </div>
                
-           </div><!-- /.loadmore-btn -->
-         </div><!-- /.posts-wrap -->
-      </div><!-- /.container -->
+           </div>
+         </div>
+      </div>
    </section>
-
- <!-- Socials -->
    <section class="section social-section" style="padding: 50px 0; background: #d9dff2">
       <div class="container">
          <div class="row">
@@ -174,12 +154,11 @@
                         </div>
                      </a>
                      </div>
-               </div><!-- /.socials-list -->
-            </div><!-- /.span12 -->
-         </div><!-- /.row -->
-      </div><!-- /.container -->
+               </div>
+            </div>
+         </div>
+      </div>
    </section>
-   <!-- Bottom -->
    <div class="bottom">
       <div class="container">
          <div class="row">
@@ -195,17 +174,13 @@
                   <a href="contact.php">CONTACT</a> |
                   <a href="#">YOUR STORE</a>
                </div>
-            </div><!-- /.span12 -->
-         </div><!-- /.row -->
-      </div><!-- /.container -->
+            </div>
+         </div>
+      </div>
    </div>
-
-   <!-- Go Top -->
    <a class="go-top">
       <i class="icons-angle-up"></i>
    </a>
-   
-   <!-- Javascript -->
    <script type="text/javascript" src="javascript/jquery.min.js"></script>
    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
    <script type="text/javascript" src="javascript/jquery.easing.js"></script>
@@ -224,7 +199,6 @@
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 	<script>
 		$(window).load(function() {
-		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");;
 	});
 	</script>
