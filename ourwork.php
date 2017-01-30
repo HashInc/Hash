a1b79b228daf9171c09658bf406fce8cf3b9a82f
Dev Sura <champroxx4596@gmail.com>
2017-01-30 14:37:58 +0530

    Comments Being REMOVED!
    OUR WORK PAGE.PHP

--- ourwork.php
+++ ourwork.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <head>
-   <!-- Basic Page Needs -->
    <meta charset="utf-8">
-    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
    <title>HashInclude | Your Web Developer</title>
    <?php 
 		$number = rand(1, 10);
@@ -20,42 +18,22 @@
 	background: url("images/loader-64x/Preloader_<?php echo $number ?>.gif") center no-repeat #fff;
 }
 </style>
-
-   <!-- Mobile Specific Metas -->
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-   <!-- Bootstrap  -->
    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
-
-   <!-- Theme Style -->
    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
-
-   <!-- Colors -->
    <link rel="stylesheet" type="text/css" href="stylesheets/colors/color4.css" id="colors">
-   
-   <!-- Animation Style -->
    <link rel="stylesheet" type="text/css" href="stylesheets/animate.css">
-
-   <!-- Google Fonts -->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
-
-   <!-- Favicon and touch icons  -->
    <link href="icon/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144">
    <link href="icon/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114">
    <link href="icon/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72">
    <link href="icon/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon-precomposed">
    <link href="icon/favicon.png" rel="shortcut icon">
-
-   <!--[if lt IE 9]>
-      <script src="javascript/html5shiv.js"></script>
-      <script src="javascript/respond.min.js"></script>
-   <![endif]-->
 </head>
 
 <body>
-   <!-- Top -->
-   <div class="se-pre-con"></div>
+    <div class="se-pre-con"></div>
    <div class="top">
       <div class="container">
          <div class="row">
@@ -63,12 +41,10 @@
                <ul class="contact-info inline pull-left">
                   <li class="email"><a href="mailto:support@hashinc.in">support@hashinc.in</a></li>
                </ul>
-            </div><!-- /.span8 -->
-         </div><!-- /.row -->
-      </div><!-- /.containter -->
+            </div>
+         </div>
+      </div>
    </div>
-
-   <!-- Parallax Fullwidth -->
    <div id="page-slider" class="parallax-bg2 parallax">
       <div class="overlay"></div>
       <header id="header" class="header">
@@ -80,10 +56,10 @@
                         <a href="./" rel="home">
                            <img src="images/logo.png" alt="Hashinc" />
                         </a>
-                     </div><!-- /.logo -->
-                  </div><!-- /.span2 -->
+                     </div>
+                  </div>
                   <div class="span10">
-                     <div class="btn-menu"></div><!-- /mobile menu button -->
+                     <div class="btn-menu"></div>
                      <nav id="mainnav" class="mainnav">
 						<ul class="menu">
                            <li><a href="index.php">Home</a>
@@ -92,16 +68,16 @@
                            <li><a class="active" href="#">Our Work</a></li>
                            <li><a href="contact.php">Contact</a></li>
                            <li><a href="store.php">YOUR STORE</a></li>
-                        </ul><!-- /.menu -->
+                        </ul>
                      </nav>
-                  </div><!-- /.span10 -->
-               </div><!-- /.row -->
-            </div><!-- /container -->
-         </div><!-- /.header-wrap -->
+                  </div>
+               </div>
+            </div>
+         </div>
       </header>
    </div>
 
-   <!-- Page Title -->
+
    <section class="page-title-container">
    	<div class="container page-title">
    		<div class="page-title-wrapper">
@@ -114,12 +90,11 @@
 					<li><a href="ourwork.php">Our Work </a>/</li>
 
 					</ul>
-			   </div><!-- /.span6 -->
+			   </div>
          </div>
-      </div><!-- /.container -->
+      </div>
    </section>
 
-   <!-- Projects -->
    <section id="projects" class="section projects-section" style="background-color: #10101d">
       <div class="container">
          <div class="row">
@@ -130,9 +105,9 @@
                   <p class="text-white">Here are some highlights of our work. We always try to be creative, different and obviously impressive.</p>
                </div>
                <div class="spacer"></div>
-            </div><!-- /.span12 -->
-         </div><!-- /.row -->
-      </div><!-- /.container -->
+            </div>
+         </div>
+      </div>
       <div class="project-wrap fullwidth">
          <ul class="project-filter v2 inline">
             <li class="active"><a data-filter="*" href="#">All</a></li>
@@ -140,7 +115,7 @@
             <li><a data-filter=".editorial" href="#">Graphic Designs</a></li>
             <li><a data-filter=".illustration" href="#">Illustration</a></li>
             <li><a data-filter=".web-design" href="#">Web Design</a></li>
-         </ul><!-- /.project-filter -->
+         </ul>
          <div class="project-container" data-portfolio-effect="zoomIn">
             <div class="item project-item branding">
                <div class="item-wrap">
@@ -160,8 +135,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item editorial">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -180,8 +155,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item illustration">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -200,8 +175,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item web-design">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -220,8 +195,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item illustration">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -240,8 +215,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item editorial branding">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -260,8 +235,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item illustration web-design">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -280,8 +255,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item branding">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -300,8 +275,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item editorial illustration">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -320,8 +295,8 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
+               </div>
+            </div>
             <div class="item project-item editorial">
                <div class="item-wrap">
                   <div class="project-thumb">
@@ -340,16 +315,15 @@
                         </div>
                      </div>
                   </div>
-               </div><!-- /.item-wrap -->
-            </div><!-- /.project-item -->
-         </div><!-- /.project-container -->
+               </div>
+            </div>
+         </div>
          <div class="loadmore-btn">
             <a href="#" class="btn-Hashinc border-type">LOAD MORE</a>
-         </div><!-- /.loadmore-btn -->
-      </div><!-- /.project-wrap -->
+         </div>
+      </div>
    </section>
 
- <!-- Socials -->
    <section class="section social-section" style="padding: 50px 0; background: #d9dff2">
       <div class="container">
          <div class="row">
@@ -373,7 +347,7 @@
                            </div>
                         </div>
                      </a>
- <a class="iconwrap" href="https://www.youtube.com/channel/UCWnwjz_S9RTms3K10NdkyTA">
+                     <a class="iconwrap" href="https://www.youtube.com/channel/UCWnwjz_S9RTms3K10NdkyTA">
                         <div class="icon">
                            <div class="iconbg">
                               <i class="icons-youtube"></i>
@@ -389,13 +363,12 @@
                         </div>
                      </a>
                      </div>
-               </div><!-- /.socials-list -->
-            </div><!-- /.span12 -->
-         </div><!-- /.row -->
-      </div><!-- /.container -->
+               </div>
+            </div>
+         </div>
+      </div>
    </section>
 
-   <!-- Bottom -->
    <div class="bottom">
       <div class="container">
          <div class="row">
@@ -412,16 +385,14 @@
                   <a href="store.php">YOUT STORE</a>
                </div>
             </div>
-         </div><!-- /.row -->
-      </div><!-- /.container -->
+         </div>
+      </div>
    </div>
 
-   <!-- Go Top -->
    <a class="go-top">
       <i class="icons-angle-up"></i>
    </a>
    
-   <!-- Javascript -->
    <script type="text/javascript" src="javascript/jquery.min.js"></script>
    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
    <script type="text/javascript" src="javascript/jquery.easing.js"></script>
@@ -440,7 +411,6 @@
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 	<script>
 		$(window).load(function() {
-		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");;
 	});
 	</script>
